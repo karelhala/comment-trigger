74f9cadef2e4284efaadf5f62994bf86af9e9d81
Karel Hala <khala@redhat.com>
2020-10-24 20:55:04 +0200

    Add basic webhooks client

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
-const { createProxyMiddleware } = require('http-proxy-middleware');
 const { join } = require('path');
+const { Webhooks } = require('@octokit/webhooks');
+const EventSource = require('eventsource');
+require('dotenv').config();
+const webhookProxyUrl = process.env.WEBHOOK_PROXY_URL;
+
+if (webhookProxyUrl) {
+    const source = new EventSource(webhookProxyUrl);
+    source.onmessage = (event) => {
+        const webhookEvent = JSON.parse(event.data);
+        webhooks
+            .verifyAndReceive({
+                id: webhookEvent['x-request-id'],
+                name: webhookEvent['x-github-event'],
+                signature: webhookEvent['x-hub-signature'],
+                payload: webhookEvent.body,
+            })
+            .catch(console.error);
+    };
+}
+
+const webhooks = new Webhooks({
+    secret: process.env.CLIENT_SECRET,
+});
+
+webhooks.on('*', ({ id, name, payload }) => {
+    console.log(name, 'event received!');
+});
 
 const app = express();
 
@@ -21,7 +47,6 @@ app.use(
     })
 );
 app.use(express.static(join(__dirname, 'build')));
-
-app.use('/probot', createProxyMiddleware({ target: 'http://localhost:1337/', changeOrigin: true }));
+app.use(webhooks.middleware);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
