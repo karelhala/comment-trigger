5c8e77646b20815d0c94876875f481710b239e7f
Karel Hala <khala@redhat.com>
2020-11-12 15:45:44 +0100

    Use in memory database when none given

--- server/db.js
+++ server/db.js
@@ -2,28 +2,51 @@ const pgp = require('pg-promise')();
 
 const getDb = (dbUrl) => pgp(dbUrl);
 
+const mem = {};
+
 module.exports = {
     init: ({ dbUrl }) => {
-        const connection = getDb(dbUrl);
-        connection.query(`CREATE TABLE IF NOT EXISTS auth_tokens (
-            username varchar(80) PRIMARY KEY,
-            token TEXT,
-            refresh_token varchar(80),
-            token_expires timestamp,
-            refresh_token_expires timestamp
-        );`);
+        let connection;
+        connection = getDb(dbUrl);
+        (async () => {
+            try {
+                await connection.query(
+                    `CREATE TABLE IF NOT EXISTS auth_tokens (
+                            username varchar(80) PRIMARY KEY,
+                            token TEXT,
+                            refresh_token varchar(80),
+                            token_expires timestamp,
+                            refresh_token_expires timestamp
+                        );`
+                );
+            } catch (e) {
+                console.log('Error while connecting to DB! Using in memory database');
+            }
+        })();
 
         return connection;
     },
-    setTokensForUser: (connection, user, token) => {
-        connection.query(
-            `INSERT INTO auth_tokens (username, token)
-        VALUES($1, $2) 
-        ON CONFLICT (username) 
-        DO
-            UPDATE SET token = $2;`,
-            [user, token]
-        );
+    setTokensForUser: async (connection, user, token) => {
+        try {
+            await connection.query(
+                `INSERT INTO auth_tokens (username, token)
+            VALUES($1, $2) 
+            ON CONFLICT (username) 
+            DO
+                UPDATE SET token = $2;`,
+                [user, token]
+            );
+        } catch (e) {
+            console.log('Error while connecting to DB! Using in memory database');
+            mem[user] = token;
+        }
     },
-    getTokensForUser: (connection, user) => connection.one(`SELECT token from auth_tokens WHERE username = $1`, [user]),
+    getTokensForUser: async (connection, user) => {
+        try {
+            return await connection.one(`SELECT token from auth_tokens WHERE username = $1`, [user]);
+        } catch (e) {
+            console.log('Error while connecting to DB! Using in memory database');
+            return { token: mem[user] };
+        }
+    },
 };
